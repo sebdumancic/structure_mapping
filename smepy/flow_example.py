3ee24497750d7ff2f480f7e3b9da66e6b4be2911
Sebastijan <sebastijan.dumancic@gmail.com>
2022-11-07 10:11:31 -0500

    added dense representation

--- smepy/flow_example.py
+++ smepy/flow_example.py
@@ -3,9 +3,9 @@ import sme
 import reader
 
 def main():
-    name, facts = reader.read_meld_file('examples/domains_altrepr/gripper_pg3.meld')
+    name, facts = reader.read_meld_file('examples/domains_dense/gripper_pg3.meld')
     water_flow = sc.StructCase(facts, name)
-    name, facts = reader.read_meld_file('examples/domains_altrepr/ferry_pg3.meld')
+    name, facts = reader.read_meld_file('examples/domains_dense/ferry_pg3.meld')
     heat_flow = sc.StructCase(facts, name)
 
     sme_1 = sme.SME(water_flow, heat_flow)
