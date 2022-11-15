a3e8d9f8acc350e1f2630a67a4974994b5ba7d30
Sebastijan <sebastijan.dumancic@gmail.com>
2022-11-15 10:53:00 -0500

    critical example

--- examples/sc_test.py
+++ examples/sc_test.py
@@ -23,7 +23,7 @@ base = [
 target = [
     ['action', 
         'sail', 
-        ['precon', ['and', ['term1', 'location', 'sail-from'], ['term1', 'location', 'sail-to'], ['term2', 'not-eq', 'sail-from', 'sail-to'], ['term1', 'at-ferry', 'sail-from']]], 
+        ['precon', ['and', ['term1', 'location', 'sail-from'], ['term2', 'not-eq', 'sail-from', 'sail-to'], ['term1', 'at-ferry', 'sail-from'], ['term1', 'location', 'sail-to']]], 
         ['effects', ['and', ['term1', 'at-ferry', 'sail-to'], ['not', ['term1', 'at-ferry', 'sail-from']]]]],
     ['action', 
         'board', 
@@ -44,6 +44,12 @@ target_sc = smepy.StructCase(target, 'target')
 
 # name, facts = smepy.read_meld_file('domains_final/ferry_pg3.meld')
 # target_alt = smepy.StructCase(facts, name)
+
+base_pr = [['action', 'move', ['precon', ['and', ['term1', 'at-robby', 'move-from'], ['term1', 'room', 'move-to'], ['term1', 'room', 'move-from']]], ['effects', ['and', ['term1', 'at-robby', 'move-to'], ['not', ['term1', 'at-robby', 'move-from']]]]], ['action', 'pick', ['precon', ['and', ['term1', 'free', 'pick-gripper'], ['term1', 'room', 'pick-room'], ['term1', 'gripper', 'pick-gripper'], ['term2', 'at', 'pick-obj', 'pick-room'], ['term1', 'at-robby', 'pick-room'], ['term1', 'ball', 'pick-obj']]], ['effects', ['and', ['term2', 'carry', 'pick-obj', 'pick-gripper'], ['not', ['term1', 'free', 'pick-gripper']], ['not', ['term2', 'at', 'pick-obj', 'pick-room']]]]], ['action', 'drop', ['precon', ['and', ['term1', 'room', 'drop-room'], ['term1', 'gripper', 'drop-gripper'], ['term2', 'carry', 'drop-obj', 'drop-gripper'], ['term1', 'at-robby', 'drop-room'], ['term1', 'ball', 'drop-obj']]], ['effects', ['and', ['term1', 'free', 'drop-gripper'], ['term2', 'at', 'drop-obj', 'drop-room'], ['not', ['term2', 'carry', 'drop-obj', 'drop-gripper']]]]]]
+base_predicators = smepy.StructCase(base_pr, 'gripper-predicators')
+
+target_pr = [['action', 'board', ['precon', ['and', ['term1', 'location', 'board-loc'], ['term0', 'empty-ferry'], ['term1', 'car', 'board-car'], ['term1', 'at-ferry', 'board-loc'], ['term2', 'at', 'board-car', 'board-loc']]], ['effects', ['and', ['term1', 'on', 'board-car'], ['not', ['term2', 'at', 'board-car', 'board-loc']], ['not', ['term0', 'empty-ferry']]]]], ['action', 'debark', ['precon', ['and', ['term1', 'on', 'debark-car'], ['term1', 'car', 'debark-car'], ['term1', 'at-ferry', 'debark-loc'], ['term1', 'location', 'debark-loc']]], ['effects', ['and', ['term2', 'at', 'debark-car', 'debark-loc'], ['term0', 'empty-ferry'], ['not', ['term1', 'on', 'debark-car']]]]], ['action', 'sail', ['precon', ['and', ['term1', 'at-ferry', 'sail-from'], ['term2', 'not-eq', 'sail-from', 'sail-to'], ['term1', 'location', 'sail-from'], ['term1', 'location', 'sail-to']]], ['effects', ['and', ['term1', 'at-ferry', 'sail-to'], ['not', ['term1', 'at-ferry', 'sail-from']]]]]]
+target_predicators = smepy.StructCase(target_pr, 'ferry-predicators')
 
 
 
